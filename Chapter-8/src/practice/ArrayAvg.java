40952ff84c74298c9c913bacf824ba177fefc88d
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-10-04 08:41:26 -0500

    Add files via upload

---
+++ Chapter-8/src/practice/ArrayAvg.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class ArrayAvg {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int[] num = {23, 6, 47, 35, 2, 14};
+		int sum = 127;
+		int avg;
+		
+		avg = sum / 2;
+		
+		System.out.println("The sum is: " + sum);
+		System.out.println("The average is: " + avg); 
+		System.out.println("The odd numbers are: " + 23 + 47 + 35);
+	}
+
+}
